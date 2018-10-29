f3e926dcf8f0e25c89540741b2c777b21cffb531
Victor Grossi <victoraugustogrossi@gmail.com>
2018-10-28 21:56:56 -0300

    juncao dos codigos, reqparser implementado na api

--- api.py
+++ api.py
@@ -1,15 +1,52 @@
+import re
+import requests
+from bs4 import BeautifulSoup
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
+
+def get_soup(url):
+    """get and parse html data from provided url"""
+    result = requests.get("https://www.pontotel.com.br/")
+    content = result.content
+    soup = BeautifulSoup(content, features='html.parser')
+    return soup
+
+def clean_text(soup):
+    """Remove script, style and formate the text properly"""
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+    #occurrences = soup.body.find_all(text=True)
+    text = soup.body.get_text()
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    return text
+
+def count_occurrences(text, word):
+    result = re.findall('Astra', text, re.IGNORECASE)
+    print(len(result))
+    return len(result)
 
 app = Flask(__name__)
 api = Api(app)
 
-class About(Resource):
-    def get(self, url):
-        return {'about' : url}
 
-api.add_resource(About, '/<string:url>')
+
+class About(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('rate', type=int, help='Rate cannot be converted')
+        parser.add_argument('name')
+        args = parser.parse_args()
+        print(args)
+        return {args.rate : args.name}
+
+api.add_resource(About, '/')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
--- crawler.py
+++ crawler.py
@@ -3,20 +3,6 @@ from bs4 import BeautifulSoup
 import re
 
 
-#def count_matches(string):
-## implementar match
-
-#def get_text_from_tag(tag):
-    # x = 0
-    # for tag in soup.body.find_all(tag, text=True):
-    #     # print(tag.text)
-    #     # count_matches(tag.text)
-    #     print(tag.text)
-    #     x = re.match('Astra', tag.text, re.IGNORECASE)        
-    # ## fazer retornar numero de matches
-    # return x
-
-
 result = requests.get("https://www.pontotel.com.br/")
 content = result.content
 soup = BeautifulSoup(content, features='html.parser')
@@ -36,14 +22,5 @@ chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
 # drop blank lines
 text = '\n'.join(chunk for chunk in chunks if chunk)
 
-#print(text)
-
-result =  re.findall('Astra', text, re.IGNORECASE)
-print(result)
+result = re.findall('Astra', text, re.IGNORECASE)
 print(len(result))
-
-#tags_to_search = ['p', 'a', 'span', 'li', 'td', 'th', 'h1', 'h2' ,'h3', 'h4', 'h5', 'h6']
-#tags_to_search = ['div']
-#matches_number = list(map(lambda tag : get_text_from_tag(tag), tags_to_search)) 
-## ajustar o map para passar dois parametros
-#print(matches_number)
