dff5c164152f915ca245b67786266ab7e6775025
Victor Grossi <victoraugustogrossi@gmail.com>
2018-10-31 18:11:20 -0300

    Get passa a receber listas de url

--- api.py
+++ api.py
@@ -7,7 +7,7 @@ from flask import Flask, Blueprint
 from flask_restplus import Resource, Api, reqparse
 
 
-def get_response_object(url): #unica funcao assincrona da API
+def get_response_object_from_url(url): #unica funcao assincrona da API
     response_object = requests.get(url)
     return response_object
 
@@ -44,19 +44,32 @@ class Crawler(Resource):
     def get(self):
         """Get how many times a provided word appears in the page"""
         parser = reqparse.RequestParser()
-        parser.add_argument('url', type=str, required=True)
+        parser.add_argument('url', action='append', type=str, required=True)
         parser.add_argument('word', type=str, required=True)
         args = parser.parse_args()
+        result = dict()
+        result['word'] = args.word
 
-        response = get_response_object(args.url)
-        html_data = get_html_from_response(response)
-        clean_html_data = remove_script_and_styles_from_html(html_data)
-        text = get_text_from_html(clean_html_data)
-        number_of_words = count_occurrences_of_word_in_text(text, args.word)
+        for url in args.url:
+            response = get_response_object_from_url(url)
+            html_data = get_html_from_response(response)
+            clean_html_data = remove_script_and_styles_from_html(html_data)
+            text = get_text_from_html(clean_html_data)
+            number_of_words = count_occurrences_of_word_in_text(text, args.word)
+            result[url] = number_of_words
+            print(number_of_words)
 
-        return {"occurrences" : number_of_words}, 200
+        return result, 200
+
+    # def post(self, url_list, word):
+    #     return {
+    #         'url_list': url_list,
+    #         'word': word
+    #     }
 
 API.add_resource(Crawler, '/api')
+
+#Criar uma classe results_in_cache(Resource): para consultar o cache com um get
 
 if __name__ == '__main__':
     APP.run(debug=True)
