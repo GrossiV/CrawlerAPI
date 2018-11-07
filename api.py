1a0c364b871f7771b37f940374615f32452fe9bb
Victor Grossi <victoraugustogrossi@gmail.com>
2018-11-07 08:31:16 -0200

    post implementado

--- api.py
+++ api.py
@@ -3,8 +3,8 @@
 import re
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask
-from flask_restplus import Resource, Api, reqparse
+from flask import Flask, Blueprint
+from flask_restplus import Resource, Api, reqparse, fields
 from flask_caching import Cache
 
 
@@ -12,20 +12,39 @@ CACHE = Cache(config={'CACHE_TYPE': 'simple'})
 APP = Flask(__name__)
 CACHE.init_app(APP)
 API = Api(APP, version='1.0', title='Crawler API', description='Count how many times a specific word appears in a webpage')
+URLS = fields.String(description='List of urls to look')
 
-@API.doc(params={
-    'url': 'List of pages to search for a specif word, e.g. "https://google.com"',
-    'word': 'Word to be searched in each item of the provided list, e.g "gmail"',
+
+word_and_urls = API.model(
+    'data', {'word': fields.String('Word to be searched'),
+    'urls': fields.List(URLS)
     })
 
-
+def get_word_occurrences_by_url(url_list, result):
+    """Funtion to orchestrate the API execution, as I have post and get methods doing the same thing,
+        with this function we can assure that no code will be repeated"""
+    for url in url_list:
+            url = url_validator(url)
+            # prevent api crash from wrong urls
+            try:
+                response = get_response_object_from_url(url)
+            except requests.exceptions.ConnectionError:
+                result[url] = 'Wrong url address'
+                continue
+            html_data = get_html_from_response(response)
+            clean_html_data = remove_script_and_styles_from_html(html_data)
+            text = get_text_from_html(clean_html_data)
+            number_of_words = count_occurrences_of_word_in_text(text, result['word'])
+            result[url] = number_of_words
+    return result
+        
 def url_validator(url):
     if not url.startswith('http'):
         url = 'http://'+ str(url)
     return url
 
 @CACHE.memoize(timeout=300)
-def get_response_object_from_url(url): #unica funcao assincrona da API
+def get_response_object_from_url(url):
     response_object = requests.get(url)
     return response_object
 
@@ -46,6 +65,12 @@ def count_occurrences_of_word_in_text(text, word):
     found_words_list = re.findall(word, text, re.IGNORECASE)
     return len(found_words_list)
 
+
+# @API.doc(params={
+#     'url': 'List of pages to search for a specif word, e.g. "https://google.com"',
+#     'word': 'Word to be searched in each item of the provided list, e.g "gmail"',
+#     })
+@API.route('/api')
 class Crawler(Resource):
     """Class to config the api access methods"""
     def get(self):
@@ -54,38 +79,21 @@ class Crawler(Resource):
         parser.add_argument('url', action='append', type=str, required=True)
         parser.add_argument('word', type=str, required=True)
         args = parser.parse_args()
-        
         result = dict()
         result['word'] = args.word
-
-        for url in args.url:
-            url = url_validator(url)
-
-            # prevent api crash from wrong urls
-            try:
-                response = get_response_object_from_url(url)
-            except requests.exceptions.ConnectionError:
-                result[url] = 'Wrong url address'
-                continue
-
-            html_data = get_html_from_response(response)
-            clean_html_data = remove_script_and_styles_from_html(html_data)
-            text = get_text_from_html(clean_html_data)
-            number_of_words = count_occurrences_of_word_in_text(text, args.word)
-            result[url] = number_of_words
-            print(number_of_words)
-
+        result = get_word_occurrences_by_url(args.url, result)
         return result, 200
 
-    # def post(self, url_list, word):
-    #     return {
-    #         'url_list': url_list,
-    #         'word': word
-    #     }
-
-API.add_resource(Crawler, '/api')
-
-#Criar uma classe results_in_cache(Resource): para consultar o cache com um get
+    @API.expect(word_and_urls)
+    def post(self):
+        # Postman headers
+        # Accept : application/json
+        # Content-Type : application/json
+        result = dict()
+        result['word'] = API.payload['word'] 
+        url_list = API.payload['urls']
+        result = get_word_occurrences_by_url(url_list, result)
+        return result, 200
 
 if __name__ == '__main__':
     APP.run(debug=True)
